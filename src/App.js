c4a7d1cebf58694169bfb061453b6e069bf2ce16
Antonio Papa <antoniopapa1991@gmail.com>
2021-12-13 17:12:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -73,7 +73,6 @@ function App() {
                                 </form>
 
                                 <ul className="list-group mb-0">
-
                                     {tasks.map(task => {
                                         return (
                                             <li className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
